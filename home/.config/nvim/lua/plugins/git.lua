b10d182e5ee486def621ded1950ee1a43dec7d67
htn <htn5299@gmail.com>
2024-12-17 16:14:52 +0700

    update

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -1,90 +1,78 @@
 return {
 	{
 		"tpope/vim-fugitive",
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
 		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{
+				"<leader>hd",
+				":Gvdiffsplit HEAD<cr>",
+				desc = "Git diff HEAD",
+			},
 		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("gitsigns").setup({
-				signs_staged_enable = true,
-				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-				watch_gitdir = {
-					follow_files = true,
-				},
-				auto_attach = true,
-				attach_to_untracked = false,
-				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 100,
-					ignore_whitespace = false,
-					virt_text_priority = 100,
-					use_focus = true,
-				},
-				current_line_blame_formatter = " 󰊢 <author>, <author_time:%R> - <summary>",
-				sign_priority = 6,
-				update_debounce = 100,
-				status_formatter = nil, -- Use default
-				max_file_length = 40000, -- Disable if file is longer than this (in lines)
-				preview_config = {
-					-- Options passed to nvim_open_win
-					border = "rounded",
-					style = "minimal",
-					relative = "cursor",
-					row = 0,
-					col = 1,
-				},
-				on_attach = function(bufnr)
-					local gitsigns = require("gitsigns")
+		opts = {
+			signs_staged_enable = true,
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+			current_line_blame_formatter = " 󰊢 <author>, <author_time:%R> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "rounded",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
 
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]h", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]g", bang = true })
+					else
+						gitsigns.nav_hunk("next")
 					end
+				end)
 
-					-- Navigation
-					map("n", "]g", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]g", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
-					end)
-
-					map("n", "[g", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[g", bang = true })
-						else
-							gitsigns.nav_hunk("prev")
-						end
-					end)
-					map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview_hunk" })
-					map("n", "<leader>hs", gitsigns.reset_hunk, { desc = "reset_hunk" })
-				end,
-			})
-		end,
+				map("n", "[h", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[g", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+				map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview_hunk" })
+				map("n", "<leader>hs", gitsigns.reset_hunk, { desc = "reset_hunk" })
+			end,
+		},
 	},
 }

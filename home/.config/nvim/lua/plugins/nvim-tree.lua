c1f35784849932dad4d5f77bd175973eeed2a997
htn <htn5299@gmail.com>
2024-12-17 19:59:29 +0700

    update

--- home/.config/nvim/lua/plugins/nvim-tree.lua
+++ home/.config/nvim/lua/plugins/nvim-tree.lua
@@ -4,19 +4,43 @@ return {
 	keys = {
 		{ "<leader>n", ":NvimTreeFindFile<cr>", "nvim-tree current file" },
 	},
-	config = {
+	opts = {
 		disable_netrw = true,
 		hijack_cursor = true,
 		update_cwd = true,
 		view = {
 			width = 30,
 			side = "left",
+			float = {
+				enable = true,
+				quit_on_focus_loss = true,
+				open_win_config = {
+					relative = "editor",
+					border = "rounded",
+					width = 45,
+					height = 40,
+					row = 1,
+					col = 1,
+				},
+			},
 		},
 		diagnostics = {
 			enable = false,
 		},
 		renderer = {
+			root_folder_label = false,
 			highlight_git = "name",
+			indent_markers = {
+				enable = true,
+				inline_arrows = true,
+				icons = {
+					corner = "└",
+					edge = "│",
+					item = "│",
+					bottom = "─",
+					none = " ",
+				},
+			},
 			icons = {
 				symlink_arrow = " → ",
 				show = {
@@ -60,9 +84,17 @@ return {
 				restrict_above_cwd = false,
 			},
 			open_file = {
-				quit_on_open = false,
+				quit_on_open = true,
 				resize_window = true,
 			},
 		},
+		on_attach = function(bufnr)
+			local api = require("nvim-tree.api")
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+			api.config.mappings.default_on_attach(bufnr)
+			vim.keymap.set("n", "f", ":lua require('fzf-lua').files()<cr>", opts("Fuzzy Find"))
+		end,
 	},
 }
